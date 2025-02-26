b902e5e79797b6d13a0515fd9568917b6d097e63
Rafael Godoy Guía <rgodoyguia@gmail.com>
2025-02-26 10:07:23 +0100

    asdf

--- src/dominio/Factorial.java
+++ src/dominio/Factorial.java
@@ -4,6 +4,6 @@ import java.util.stream.IntStream;
 
 public class Factorial {
     public static int factorial (int n) {
-        return (IntStream.rangeClosed(1, n).reduce((num1, num2) -> num1 * num2).orElse(0));
+        return (IntStream.rangeClosed(1, n).reduce((num1, num2) -> num1 * num2).orElse(1));
     }
 }
--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,10 +1,11 @@
 package src.principal;
 
 import src.dominio.Factorial;
+import src.dominio.PotenciaNesima;
 import src.dominio.Suma123n;
 
 public class Main {
     public static void main (String[] argv) {
-        System.out.println(Factorial.factorial(3));
+        System.out.println(PotenciaNesima.potenciaNesima(2, 4));
     }
 }
