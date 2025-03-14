330ff26b59c907e09e72ab8c97b3380923d2d54b
Rafael Godoy Guía <rgodoyguia@gmail.com>
2025-03-14 08:41:29 +0100

    Estructura tests

--- src/dominio/Matematicas.java
+++ src/dominio/Matematicas.java
@@ -117,3 +117,5 @@ public class Matematicas {
                 .toList();
     }
 }
+
+
---
+++ test/dominio/MatematicasTest.java
@@ -0,0 +1,74 @@
+package test.dominio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatematicasTest {
+
+    @org.junit.jupiter.api.Test
+    void obtenerListaPar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaParesLista() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void potencia() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaNumerosConsecutivos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaParesHasta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void factorial() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void media() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void desviacionTipica() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generarSecuenciaOf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generarSecuenciaRange() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generarSecuenciaIterate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generarNumerosAleatorios() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void productoEscalar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void integral() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mostrarFibonacci() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void terminoFibonacci() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generarPares() {
+    }
+}
